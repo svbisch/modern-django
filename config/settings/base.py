f08e19a1876a7008e13c84eab85eee00f24925c4
Sven Bischoff <sven.bischoff@charite.de>
2017-06-02 14:43:20 +0200

    Complete Part 2 of Modern Django tutorial

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,9 +52,11 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
+    'rest_framework',
 )
 
 LOCAL_APPS = (
+    'project.api',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -153,3 +155,6 @@ STATICFILES_FINDERS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = str(APPS_DIR('media'))
+
+REST_FRAMEWORK = {
+}
